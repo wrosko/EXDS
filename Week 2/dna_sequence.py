6e8e3e4d3252a51074ecee9844bafc6bdadc4b66
wrosko <wrosko@bates.edu>
2015-05-07 15:56:56 -0400

    dna sequence

--- Week 2/dna_sequence.py
+++ Week 2/dna_sequence.py
@@ -1,18 +1,22 @@
 
 
-seqletters = 'ACTG'
-
 string1 = raw_input('Please enter the DNA sequence for string 1: ').upper()
-print seqletters
 print string1
+result = [string1.strip('ACTG')]
+print
+print
+if result == ['']:      #Make a while loop here
+    print
+else:
+    print 'invalid sequence'
+    
+string2 = raw_input('Please enter the DNA sequence for string 2: ').upper()
+print string2
+result = [string2.strip('ACTG')]
+print
+print
+if result == ['']:      #Make a while loop here
+    print
+else:
+    print 'invalid sequence'
 
-if not(any(seqletters in string1)):
-        print 'String 1 is not a valid sequence'
-        
-#string2 = raw_input('Please enter the DNA sequence for string 2: ').upper()
-#if not("A" in string1) or not("C" in string1) or not("T" in string1) or not("G" in string1):
-  #  print 'Not a valid DNA sequence for string 1'
-#elif not("A" in string2) or not("C" in string2) or not("T" in string2) or not("G" in string2):
-  #  print 'Not a valid DNA sequence for string 2'
-#else:
- #   print 'hi'
